6a9ce3c615f38a1fa31887ee6864f9d89c6534cf
Yossef horvitz <yossefhorvitz@gmail.com>
2025-12-07 16:00:01 +0200

    with logging

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
-mod models;
 mod db;
 mod handlers;
+mod models;
 mod router;
 
-use axum::serve;
-use tokio::net::TcpListener;
 use crate::db::init_db;
 use crate::router::create_router;
+use axum::serve;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
     let db = init_db();
     let app = create_router(db);
 
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    tracing::info!("starting server!!");
     println!("Server running at http://127.0.0.1:3000");
-
     serve(listener, app).await.unwrap();
 }
